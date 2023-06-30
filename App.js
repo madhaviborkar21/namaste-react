1763ef3aaf00982fa0be36e6979d081c7238da7b
madhavi <zen27428@adobe.com>
2023-06-30 16:04:58 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,14 @@
+const parent = React.createElement( "div", {id: "parent"},[
+    React.createElement( "div", {id :"child"}, [
+        React.createElement( "h1",{},"I am h1 tag"),
+        React.createElement( "h2",{},"I am h2 tag")
+    ]),
+    React.createElement( "div", {id :"child2"}, [
+        React.createElement( "h1",{},"I am h1 tag"),
+        React.createElement( "h2",{},"I am h2 tag")
+    ]),
+]);
+console.log(parent)
+// const heading = React.createElement("h1", {id: "heading", class: "abc",}, "Hello world from React !");
+const root  = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
